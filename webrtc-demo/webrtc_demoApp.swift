d9fd5f0733e1fdc91ca13e3941a8920f85f8ef82
lzz <793319302@qq.com>
2026-02-25 22:50:23 +0800

    Initial Commit

---
+++ webrtc-demo/webrtc_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  webrtc_demoApp.swift
+//  webrtc-demo
+//
+//  Created by lizhengze on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct webrtc_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
